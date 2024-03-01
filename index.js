43348e1c5fe043d2cdcb96b0f1226d65d8456d52
abhilash44444 <145905973+abhilash44444@users.noreply.github.com>
2024-03-01 13:54:27 +0000

    firtst commit

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("server is running at the port 3000");
 });
 
 app.listen(3000, () => {
-  console.log('Express server initialized');
+  console.log("Express server initialized");
 });
